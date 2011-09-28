5e5d3157b994011362edd2da584e1c8bbb7dbd6a
Harry Bullen <hbullen@gmail.com>
2011-09-28 01:49:14 -0400

    Added a minmax player at depth 1

---
+++ players/MinMax_Player.java
@@ -0,0 +1,137 @@
+package players;
+
+import game.Board;
+import game.Move;
+
+import java.util.Random;
+import java.util.Vector;
+
+/**
+ * A player that uses MinMax for the game of Quoridor.
+ */
+public class MinMax_Player implements Player {
+	private Random rng;
+	private Player_ID self_id;
+	private static Player_ID[] players_ids = new Player_ID[]{Player_ID.PLAYER_1, Player_ID.PLAYER_2, Player_ID.PLAYER_3, Player_ID.PLAYER_4};
+	
+	int LOSS = -10000;
+	int TIE =       0;
+	int WIN =   10000;
+
+	/**
+	 * The player, whose turn it is next.
+	 */
+	public static Player_ID next_player( Player_ID pid) {
+		return players_ids[ (pid.ordinal() + 1) % 4 ];
+	}
+
+	public MinMax_Player(){
+		rng = new Random();
+	}
+
+	/**
+	 * returns a random move choice
+	 */
+	public Move make_move(Board b) {
+		Vector<Move> moves = b.get_possible_moves(self_id);
+		Vector<Move> good_moves = new Vector<Move>();
+		int best = LOSS;
+		for ( int i=0 ; i < moves.size(); i++) {
+			int[] eval = eval_move( b, moves.get(i), 1);
+			int myScore = eval[self_id.ordinal()];
+			if ( myScore > best) {
+				best = myScore;
+				good_moves.removeAllElements();
+				good_moves.add(moves.get(i) );
+			} else if( myScore == best ) {
+				good_moves.add(moves.get(i));
+			}
+		}
+		for ( int i= 0 ; i < good_moves.size() ; i++) {
+			System.out.println( good_moves.get(i) );
+		}
+		System.out.println("=====================================" );
+		return good_moves.get(rng.nextInt(good_moves.size()));
+	}
+
+	/**
+	 * new move
+	 */
+	public int[] eval_move(Board board, Move m, int depth) {
+		// use this one so the orignal isn't modified
+		//System.out.println( m );
+		Board b = new Board( board );
+		b.apply_move( m );
+
+		if (depth == 0) {
+			return eval_board( b);
+		} //else 
+		int[] alpha = { LOSS, LOSS, LOSS, LOSS };
+		// get moves by next player
+		Player_ID p = next_player(m.getPlayer_making_move() );
+		Vector<Move> moves = b.get_possible_moves( p );
+		for ( int i=0; i < moves.size(); i++ ) {
+			int[] eval = eval_move( b , moves.get(i), depth -1 );
+			if ( alpha[p.ordinal()] < eval[p.ordinal()] )
+				alpha = eval;
+		}
+		return alpha;
+	}
+
+	public int[] eval_board(Board b){
+
+		// if game is over
+		if ( b.is_game_over() ) {
+			Player_ID p = b.compute_winner();
+			if (p == null) {
+				int[] eval = { TIE, TIE, TIE, TIE};
+				return  eval;
+			} else {
+				int[] eval = { LOSS, LOSS, LOSS, LOSS};
+				eval[p.ordinal()] = WIN;
+				return eval;
+			}
+		}
+
+		// other wise
+		int shortp[] = new int[4];
+
+		for ( int i = 0; i < 4; i++) {
+			// negate shortest path so shorter paths score higher
+			shortp[i]= - b.shortest_path(players_ids[i]);
+		}
+
+		// eval is mine - next best player
+		int[] eval = { WIN, WIN, WIN, WIN };
+		for ( int i = 0; i < 4; i++){
+			for (int j = 0 ;j < 4; j++) {
+				if( i != j && shortp[i] - shortp[j] < eval[i] )
+					eval[i] = shortp[i] - shortp[j];
+			}
+		}
+		return eval;
+	}
+
+
+	/**
+	 * ignores move notifications
+	 */
+	public void notify_of_move(Player_ID playerThatMadeMove, Move moveMade,
+			Board resultingBoard) {
+		// no code
+	}
+
+	/**
+	 * sets this players id for use with the game engine
+	 */
+	public void set_id(Player_ID id){
+		self_id = id;
+	}
+
+	/**
+	 * sets the random seed
+	 */
+	public void set_seed(long seed){
+		rng.setSeed(seed);
+	}
+}
