4bdad3dcf22ca7a6193ad1b2b2698a6e17986f80
Harry Bullen <hbullen@gmail.com>
2011-10-09 21:34:50 -0400

    Stable driver that selects game type

--- driver/ExampleGame.java
+++ driver/ExampleGame.java
@@ -21,29 +21,63 @@ public class ExampleGame {
 	public static void main(String[] args){
 		// use a small seed by default
 		long seed = System.currentTimeMillis() % 10000;
+		int game_type = 1; // easy mix
 		// parse arguments poorly
 		if (args.length > 0) {
 			try {
-				seed = Long.parseLong(args[0]);
+				game_type = Integer.parseInt(args[0]);
 			} catch (NumberFormatException e) {
 				System.err.println("Argument must be a integer");
 				System.exit(1);
 			}
 		}
+		if (args.length > 1) {
+			try {
+				seed = Long.parseLong(args[1]);
+			} catch (NumberFormatException e) {
+				System.err.println("Argument must be a integer");
+				System.exit(1);
+			}
+		}
+		System.out.println ("Game type: " + game_type );
 		System.out.println ("Using seed: " + seed );
 
 		Vector<Player> players = new Vector<Player>();
 		Random rng = new Random( seed );
 		
 		// Add 4 players
-		//players.add(new Random_Player());
-		//players.add(new Wall_Follow_Player());
-		//players.add(new OneAhead_Player());
-		players.add(new MinMax_Player());
-		players.add(new AlphaBeta_Player());
-		players.add(new MinMax_Player());
-		players.add(new OneAhead_Player());
-		
+		switch( game_type ) {
+			case 0:	// random
+				players.add(new Random_Player());
+				players.add(new Random_Player());
+				players.add(new Random_Player());
+				players.add(new Random_Player());
+			case 1: // easy mix
+				players.add(new Random_Player());
+				players.add(new Wall_Follow_Player());
+				players.add(new OneAhead_Player());
+				players.add(new Wall_Follow_Player());
+				break;
+			case 2: // one Ahead (quick)
+				players.add(new OneAhead_Player());
+				players.add(new OneAhead_Player());
+				players.add(new OneAhead_Player());
+				players.add(new OneAhead_Player());
+				break;
+			case 3: // good mix (slow)
+				players.add(new MinMax_Player());
+				players.add(new OneAhead_Player());
+				players.add(new MinMax_Player());
+				players.add(new OneAhead_Player());
+				break;
+			case 4: // minMax (slow)
+				players.add(new MinMax_Player());
+				players.add(new MinMax_Player());
+				players.add(new MinMax_Player());
+				players.add(new MinMax_Player());
+				break;
+		}
+
 		for(int i = 0; i < 4; i++){
 			players.get(i).set_seed( rng.nextLong() );
 		}
@@ -56,6 +90,7 @@ public class ExampleGame {
 		else{
 			System.out.format("%s wins.\n", winner);
 		}
+		System.out.println ("Game type: " + game_type );
 		System.out.println ("Seed: " + seed );
 	}
 }
