0c094bb6f2886a87bbda60514bbb84360fabd619
Harry Bullen <hbullen@gmail.com>
2011-10-15 15:05:42 -0400

    Maded MinMax Player use threads

--- players/MinMax_Player.java
+++ players/MinMax_Player.java
@@ -14,9 +14,9 @@ public class MinMax_Player implements Player {
 	protected Player_ID self_id;
 	protected static Player_ID[] players_ids = new Player_ID[]{Player_ID.PLAYER_1, Player_ID.PLAYER_2, Player_ID.PLAYER_3, Player_ID.PLAYER_4};
 	
-	protected int LOSS = -10000;
-	protected int TIE =       0;
-	protected int WIN =   10000;
+	public int LOSS = -10000;
+	public int TIE =       0;
+	public int WIN =   10000;
 
 	// number of board evaluated
 	protected int evaluated = 0;
@@ -41,26 +41,26 @@ public class MinMax_Player implements Player {
 		Vector<Move> good_moves = (Vector)moves.clone();
 		// Increment to zero at start of loop
 		int depth = -1;
+		int MAX_TIME = 1995; // in milliseconds
 		evaluated = 0;
 		// Iterative deepening
-		while ( System.currentTimeMillis() - start < 1000 )
+		while ( System.currentTimeMillis() - start < MAX_TIME )
 		{
 			depth += 1;
-			// new list of good moves
-			good_moves.removeAllElements();
-			int best = LOSS;
-			for ( int i=0 ; i < moves.size(); i++) {
-				// eval
-				int[] eval = eval_move( b, moves.get(i), depth);
-				int myScore = eval[self_id.ordinal()];
-				if ( myScore > best) {
-					best = myScore;
-					good_moves.removeAllElements();
-					good_moves.add(moves.get(i) );
-				} else if( myScore == best ) {
-					good_moves.add(moves.get(i));
+			Vector<Move> returned_moves = new Vector<Move>();
+			try {
+				MinMaxThread _mx = new MinMaxThread(returned_moves, new Board(b), depth );
+				_mx.start();
+				synchronized (_mx) {
+					_mx.join(MAX_TIME - (System.currentTimeMillis() - start) );
+					// we are out of time
+					if ( _mx.isAlive() ) { 
+						_mx.interrupt();
+					}
+					else
+						good_moves = (Vector)returned_moves.clone();
 				}
-			}
+			} catch (InterruptedException e ) { }
 		}
 		// Print moves
 		/*
@@ -68,76 +68,109 @@ public class MinMax_Player implements Player {
 			System.out.println( good_moves.get(i) );
 		}*/
 		System.out.println("Depth: " + depth );
-		System.out.println("Evaluated: " + evaluated + " " +
-			evaluated / ((System.currentTimeMillis() -start) / 1000.0 ) + "eval/sec" );
+		//System.out.println("Evaluated: " + evaluated + " " +
+			//evaluated / ((System.currentTimeMillis() -start) / 1000.0 ) + "eval/sec" );
 		System.out.println("=====================================" );
 		return good_moves.get(rng.nextInt(good_moves.size()));
 	}
 
-	/**
-	 * new move
-	 */
-	public int[] eval_move(Board board, Move m, int depth) {
-		// use this one so the orignal isn't modified
-		//System.out.println( m );
-		// Generate target node
-		Board b = new Board( board );
-		b.apply_move( m );
-		// Should check for game over here
+	class MinMaxThread extends Thread {
+		Vector<Move> best_moves;
+		Board b;
+		int depth;
+		int evaluated;
 
-		// Check for depth limit
-		if (depth == 0) {
-			return eval_board( b);
-		} //else 
-
-		// Do minMax of kids
-		int[] alpha = { LOSS, LOSS, LOSS, LOSS };
-		// get moves by next player
-		Player_ID p = next_player(m.getPlayer_making_move() );
-		Vector<Move> moves = b.get_possible_moves( p );
-		for ( int i=0; i < moves.size(); i++ ) {
-			int[] eval = eval_move( b , moves.get(i), depth -1 );
-			// Check if this move gives next player better pos
-			if ( alpha[p.ordinal()] < eval[p.ordinal()] )
-				alpha = eval;
+		public MinMaxThread( Vector<Move> ret, Board board, int d) {
+			this.best_moves = ret;
+			this.b = board;
+			this.depth = d;
 		}
-		return alpha;
-	}
 
-	public int[] eval_board(Board b){
-
-		evaluated += 1;
-		// if game is over
-		if ( b.is_game_over() ) {
-			Player_ID p = b.compute_winner();
-			if (p == null) {
-				int[] eval = { TIE, TIE, TIE, TIE};
-				return  eval;
-			} else {
-				int[] eval = { LOSS, LOSS, LOSS, LOSS};
-				eval[p.ordinal()] = WIN;
-				return eval;
+		
+		public void run() {
+			// new list of good moves
+			best_moves.removeAllElements();
+			Vector<Move> moves = b.get_possible_moves(self_id);
+			int best = LOSS;
+			for ( int i=0 ; !isInterrupted() && i < moves.size(); i++) {
+				// eval
+				int[] eval = eval_move( b, moves.get(i), depth);
+				int myScore = eval[self_id.ordinal()];
+				if ( myScore > best) {
+					best = myScore;
+					best_moves.removeAllElements();
+					best_moves.add(moves.get(i) );
+				} else if( myScore == best ) {
+					best_moves.add(moves.get(i));
+				}
 			}
 		}
 
-		// other wise
-		int shortp[] = new int[4];
+		/**
+		 * new move
+		 */
+		public int[] eval_move(Board board, Move m, int depth) {
+			// use this one so the orignal isn't modified
+			//System.out.println( m );
+			// Generate target node
+			Board b = new Board( board );
+			b.apply_move( m );
+			// Should check for game over here
 
-		for ( int i = 0; i < 4; i++) {
-			// negate shortest path so shorter paths score higher
-			shortp[i]= - b.shortest_path(players_ids[i]);
-			// TODO: add number of walls left
-		}
+			// Check for depth limit
+			if (depth == 0) {
+				return eval_board( b);
+			} //else 
 
-		// eval is mine - next best player
-		int[] eval = { WIN, WIN, WIN, WIN };
-		for ( int i = 0; i < 4; i++){
-			for (int j = 0 ;j < 4; j++) {
-				if( i != j && shortp[i] - shortp[j] < eval[i] )
-					eval[i] = shortp[i] - shortp[j];
+			// Do minMax of kids
+			int[] alpha = { LOSS, LOSS, LOSS, LOSS };
+			// get moves by next player
+			Player_ID p = next_player(m.getPlayer_making_move() );
+			Vector<Move> moves = b.get_possible_moves( p );
+			for ( int i=0; i < moves.size(); i++ ) {
+				int[] eval = eval_move( b , moves.get(i), depth -1 );
+				// Check if this move gives next player better pos
+				if ( alpha[p.ordinal()] < eval[p.ordinal()] )
+					alpha = eval;
 			}
+			return alpha;
 		}
-		return eval;
+
+		public int[] eval_board(Board b){
+
+			evaluated += 1;
+			// if game is over
+			if ( b.is_game_over() ) {
+				Player_ID p = b.compute_winner();
+				if (p == null) {
+					int[] eval = { TIE, TIE, TIE, TIE};
+					return  eval;
+				} else {
+					int[] eval = { LOSS, LOSS, LOSS, LOSS};
+					eval[p.ordinal()] = WIN;
+					return eval;
+				}
+			}
+
+			// other wise
+			int shortp[] = new int[4];
+
+			for ( int i = 0; i < 4; i++) {
+				// negate shortest path so shorter paths score higher
+				shortp[i]= - b.shortest_path(players_ids[i]);
+				// TODO: add number of walls left
+			}
+
+			// eval is mine - next best player
+			int[] eval = { WIN, WIN, WIN, WIN };
+			for ( int i = 0; i < 4; i++){
+				for (int j = 0 ;j < 4; j++) {
+					if( i != j && shortp[i] - shortp[j] < eval[i] )
+						eval[i] = shortp[i] - shortp[j];
+				}
+			}
+			return eval;
+		}
 	}
 
 
